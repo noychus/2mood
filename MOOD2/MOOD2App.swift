2439202c44467cceb7eac2471af1209bdedf80e7
noychus <157799428+noychus@users.noreply.github.com>
2024-10-09 13:25:36 +0300

    Create Project

---
+++ MOOD2/MOOD2App.swift
@@ -0,0 +1,17 @@
+//
+//  MOOD2App.swift
+//  MOOD2
+//
+//  Created by NOY on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MOOD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
